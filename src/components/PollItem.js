3f60d6b297bd07e6fbc47798d455ddc242f3991a
Ben Harrison <benthedeveloper@users.noreply.github.com>
2026-04-13 15:22:10 -0700

    feat: PollItem component set up to render question data

--- src/components/PollItem.js
+++ src/components/PollItem.js
@@ -6,7 +6,30 @@ const PollItem = ({ question, questionAuthor }) => {
 
   return (
     <li className="poll-item">
-      Poll Item
+      <div>
+        <div className="user-info">
+          <img className="avatar-img" src={questionAuthor.avatarURL} alt="" />
+          <div>
+            <div className="user-name">{questionAuthor.name}</div>
+            <div>
+              <span>{new Date(question.timestamp).toLocaleDateString()}</span>
+              <span> | </span>
+              <span>{new Date(question.timestamp).toLocaleTimeString()}</span>
+            </div>
+          </div>
+        </div>
+        <div className="question-info">
+          <div>Would you rather:</div>
+          <ul>
+            <li>{question.optionOne.text}</li>
+            <li>{question.optionTwo.text}</li>
+          </ul>
+        </div>
+      </div>
+      <div className="button-container">
+        {/* TODO change this to a Link - to questions/:questionId after setting up route */}
+        <button className="btn">View</button>
+      </div>
     </li>
   );
 };
