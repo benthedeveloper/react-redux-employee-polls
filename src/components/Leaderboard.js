c7b1ea8922c6347c9b28707a3b792ef61f1c59cf
Ben Harrison <benthedeveloper@users.noreply.github.com>
2026-04-15 16:31:59 -0700

    feat: Implement Leaderboard component

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,6 +1,49 @@
+import { useSelector } from 'react-redux';
+import AppHeader from './AppHeader';
+
 const Leaderboard = () => {
+  const users = useSelector((state) => state.users);
+
+  // TODO document this method
+  const getNumPollsCreated = (user) => {
+    return user.questions?.length || 0;
+  };
+
+  // TODO document this method
+  const getNumPollsAnswered = (user) => {
+    return Object.keys(user.answers)?.length || 0;
+  };
+
+  const sortedUsersArray = Object.values(users).sort((a, b) => {
+    const aScore = getNumPollsAnswered(a) + getNumPollsCreated(a);
+    const bScore = getNumPollsAnswered(b) + getNumPollsCreated(b);
+    return bScore - aScore;
+  });
+
+  // TODO need to add AppHeader and container for page content
   return (
-    <div>Leaderboard component</div>
+    <>
+      <AppHeader />
+      <div className="page-content">
+        <div className="leaderboard">
+          <h2>Leaderboard</h2>
+          <ol className="leaderboard-list">
+            {sortedUsersArray.map((user) => (
+              <li key={user.id} className="leaderboard-list-item">
+                <div className="user-info">
+                  <img src={user.avatarURL} alt="" className="avatar-img" />
+                  <span>{user.name}</span>
+                </div>
+                <div className="polls-info">
+                  <span>Polls created: {getNumPollsCreated(user)}</span>
+                  <span>Polls answered: {getNumPollsAnswered(user)}</span>
+                </div>
+              </li>
+            ))}
+          </ol>
+        </div>
+      </div>
+    </>
   );
 };
 
