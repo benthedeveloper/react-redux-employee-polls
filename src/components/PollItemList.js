82c1c850ed05181597fec277e25d6e30c67caede
Ben Harrison <benthedeveloper@users.noreply.github.com>
2026-04-15 17:26:46 -0700

    fix: Sort poll items on Dashboard by timestamp

--- src/components/PollItemList.js
+++ src/components/PollItemList.js
@@ -1,9 +1,11 @@
 import PollItem from './PollItem';
 
 const PollItemList = ({ questions }) => {
+  const sortedQuestions = questions.sort((a, b) => b.timestamp - a.timestamp);
+  
   return (
     <ul className="poll-item-list">
-      {questions.map((question) => (
+      {sortedQuestions.map((question) => (
         <PollItem key={question.id} question={question} />
       ))}
     </ul>
