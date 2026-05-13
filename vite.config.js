8191907f4938410c0da9555ae274d0b68c98c153
Ben Harrison <benthedeveloper@users.noreply.github.com>
2026-05-13 12:03:47 -0700

    fix: Fix typo in vite config

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
     base: env.VITE_BASE_PATH,
     plugins: [react()],
     optimizeDeps: {
-      include: ['react, react-redux'],
+      include: ['react', 'react-redux'],
     },
     test: {
       projects: [
